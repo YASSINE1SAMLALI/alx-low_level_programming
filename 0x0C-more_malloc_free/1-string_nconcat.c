b29f02969305b7a5aaf5747613a1f0463015dcc5
YASSINE1SAMLALI <essamlali.yassine.93@gmail.com>
2023-04-13 11:25:48 -0700

    string contact

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - a function that concatenates string with n bytes
- * @s1: destination for concatenation
- * @s2: source of string
- * @n: int type for size of byte
- * Return: pointer to new memory allocated
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of s2
+ * Return: pointer allocate space in memory
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, i1;
-	int s = n;
 	char *ptr;
-	int len1, len2;
+	unsigned int a, b, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	for (a = 0; s1[a] != '\0'; a++)
 		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
+	for (b = 0; s2[b] != '\0'; b++)
 		;
+	if (n > b)
+	n = b;
+	c = a + n;
+	ptr = malloc(c + 1);
 
-	if (s >= len2)
-	{
-		sign = len2;
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
-	else
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-	{
-		ptr[i] = s1[i];
-	}
-	for (i1 = 0; i1 < s; i1++)
-	{
-		ptr[i++] = s2[i1];
-	}
-	ptr[count++] = '\0';
+	for (d = 0; d < c; d++)
+
+		if (d < a)
+		ptr[d] = s1[d];
+		else
+		ptr[d] = s2[d - a];
+	ptr[c] = '\0';
 	return (ptr);
 }
