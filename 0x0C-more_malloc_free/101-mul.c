0c590c995a60034cc9f84e4006d5c3c5d3addce1
YASSINE1SAMLALI <essamlali.yassine.93@gmail.com>
2023-04-14 18:39:52 -0700

    more malloc advanced

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+
